39bf36c5883a428cf5966a33fe50b221f27809ef
Mahan Bayhaghi <mahan.bayhaghi@gmail.com>
2023-06-01 10:41:27 +0330

    shit

--- predictor.v
+++ predictor.v
@@ -1,17 +1,14 @@
 
 // vaghti result = 1 , taken mana dar mishe
 
-
 module predictor(input wire request, result, clk, taken, output reg prediction);
 
-  reg [1:0] counter;       // 2-bit saturating counter
+  reg [1:0] counter = 2'b11;       // 2-bit saturating counter
   
   always @(posedge clk) begin
     // predict
     if (request) begin
-      if (counter >= 2)
-        prediction <= 1;
-    else prediction <= 0;
+      prediction = counter[1];
     end
     if (reslut) begin
       if (taken) begin
