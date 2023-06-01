997ae8e386698a583e83679ad86ec6d849e5d5b9
Mahan Bayhaghi <mahan.bayhaghi@gmail.com>
2023-06-01 10:37:29 +0330

    first code commit

--- predictor.v
+++ predictor.v
@@ -1,10 +1,25 @@
 
+// vaghti result = 1 , taken mana dar mishe
+
 
 module predictor(input wire request, result, clk, taken, output reg prediction);
 
-// Your code
-
+  reg [1:0] counter;       // 2-bit saturating counter
   
-
-
+  always @(posedge clk) begin
+    // predict
+    if (request) begin
+      if (counter >= 2)
+        prediction <= 1;
+    else prediction <= 0;
+    end
+    if (reslut) begin
+      if (taken) begin
+        if (counter != 3) counter <= counter + 1
+      end
+    else begin
+      if (counter != 0) counter <= counter - 1
+    end
+  end 
 endmodule
+
